45329972d20c934350b01166c69b4561f357481d
Markus Meyer <meyer_markus@gmx.de>
2021-07-27 14:33:24 +0200

    ServiceBus

--- PresenceWebhook/GraphWebhook/__init__.py
+++ PresenceWebhook/GraphWebhook/__init__.py
@@ -6,10 +6,10 @@ import json
 def main(req: func.HttpRequest, msgOut: func.Out[str], context: func.Context)-> func.HttpResponse: 
     
     logging.info('Python HTTP trigger function processed a request.')
+    validationToken = req.params.get('validationToken')
     req_body = req.get_json()
     input_msg = json.dumps(req_body)
-    # logging.info(input_msg)
-
+    logging.info(validationToken)
 
     msgOut.set(input_msg)
-    return func.HttpResponse("OK")
+    return func.HttpResponse(validationToken)
