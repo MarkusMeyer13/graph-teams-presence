afed5b0b42966728134566dd56fd1116b16e291e
Markus Meyer <meyer_markus@gmx.de>
2021-07-27 11:07:07 +0200

    ServiceBus

--- PresenceWebhook/GraphWebhook/__init__.py
+++ PresenceWebhook/GraphWebhook/__init__.py
@@ -5,8 +5,8 @@ import azure.functions as func
 def main(req: func.HttpRequest, msgOut: func.Out[str], context: func.Context)-> func.HttpResponse: 
     
     logging.info('Python HTTP trigger function processed a request.')
-    
-    input_msg = req.params.get('message')
+    req_body = req.get_json()
+    input_msg = req_body
     logging.info(input_msg)
 
     my_app_setting_value = os.environ["ServiceBusConnection"]
